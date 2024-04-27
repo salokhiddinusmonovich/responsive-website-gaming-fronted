50b0946bdf446c306f0cab1924b6673c7ea70b6d
Salokhiddin <157391066+usmonovichss@users.noreply.github.com>
2024-04-27 21:35:08 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const sr = ScrollReveal({
+    distance: '40px',
+    duration: 2400,
+    reset: true
+});
+
+
+
+sr.reveal('.hero-text span', {delay:400});
+sr.reveal('.hero-img', {delay:500, origin: 'top'});
+sr.reveal('.hero-text h5', {delay:600, origin: 'bottom'});
+sr.reveal('.hero-text h1', {delay:700, origin: 'bottom'});
+sr.reveal('.hero-text p', {delay:800, origin: 'top'});
+sr.reveal('.hero-btn', {delay:900, origin: 'top'});
+
+
+sr.reveal('.imges-box', {delay:1000, origin: 'top'});
+sr.reveal('.social-icons', {delay:1100, origin: 'bottom'});
+sr.reveal('.scroll-btn', {delay:1150, origin: 'top'});
+
+
+
+
+
+
